79f64129af0603664a5565fd8ec209e3de776683
ram <rramkumar1306@gmail.com>
2024-05-23 23:46:42 +0530

    Introduced updateRotation method.

--- Scripts/Dice.gd
+++ Scripts/Dice.gd
@@ -1,8 +1,15 @@
 extends RigidBody3D
 
+@onready var pivot = $Pivot
+
 var axes = {
 	1 : 270,
 	3 : 0,
-	6 : 180,
+	0 : 180,
 	2 : 90  
 }
+var animDuration = 1
+
+func updateRotation(key):
+	var tween = get_tree().create_tween()
+	tween.tween_property(pivot, "rotation", Vector3(0, 0, axes[key]), animDuration)
