66b8e99e6dc72c90ef1663ff55bc3a3014c20974
ram <rramkumar1306@gmail.com>
2024-05-24 18:25:31 +0530

    Implemented die roll functinality.

--- Scripts/Dice.gd
+++ Scripts/Dice.gd
@@ -6,9 +6,14 @@ extends RigidBody3D
 func roll():
 	sleeping = false
 	freeze = false
-	transform.origin = StartPosition
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "rotation", Vector3(0, 0, 0), 0.3)
+	tween.tween_property(self, "position", StartPosition, 0.5)
+#	transform.origin = StartPosition
 	linear_velocity = Vector3.ZERO
 	angular_velocity = Vector3.ZERO
 	transform.basis = Basis(Vector3.RIGHT, randf_range(0, 2 * PI)) * transform.basis
 	
 	var throwVector = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
+	angular_velocity = throwVector * rollStrength / 2
+	apply_central_impulse(throwVector * rollStrength)
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,13 +1,9 @@
 extends Node3D
 
-var diceScene = preload("res://Scenes/Dice.tscn")
+@onready var dice = $Dice
+@onready var dice2 = $Dice2
 
 func _on_button_pressed():
-	var numbers = Helpers.getRandomNumbers()
-	for number in numbers:
-		var dice = diceScene.instantiate()
-		var x = randi_range(-5, 5)
-		var y = randi_range(-5, 5)
-		dice.position = Vector3(x, 5, y)
-		add_child(dice)
-		dice.updateRotation(numbers[number])
+	dice.roll()
+	dice2.roll()
+	
