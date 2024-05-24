23f623e3317d7cc6cd62056a9853972e2ec15371
ram <rramkumar1306@gmail.com>
2024-05-24 21:03:47 +0530

    Added dice animation.

--- Scripts/Dice.gd
+++ Scripts/Dice.gd
@@ -2,6 +2,7 @@ extends RigidBody3D
 
 @export var StartPosition: Vector3 = Vector3.ZERO
 @export var rollStrength: int = 30
+@export var level: Node3D
 @onready var rayCasts = $CollisionShape3D/RayCasts
 
 signal rollFinished(die: RigidBody3D, number: int)
@@ -11,18 +12,20 @@ func roll():
 	freeze = false
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "rotation", Vector3(0, 0, 0), 0.3)
-	tween.tween_property(self, "position", StartPosition, 0.5)
+	tween.tween_property(self, "position", StartPosition, 0.3)
 
 	linear_velocity = Vector3.ZERO
 	angular_velocity = Vector3.ZERO
+	transform.basis = Basis(Vector3.UP, randf_range(0, 2 * PI)) * transform.basis
 	transform.basis = Basis(Vector3.RIGHT, randf_range(0, 2 * PI)) * transform.basis
+	transform.basis = Basis(Vector3.FORWARD, randf_range(0, 2 * PI)) * transform.basis
 	
 	var throwVector = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
 	angular_velocity = throwVector * rollStrength / 2
 	apply_central_impulse(throwVector * rollStrength)
 
 func _on_sleeping_state_changed():
-	if (sleeping):
+	if sleeping and level.isRolling :
 		var landedOnSide = false
 		for raycast in rayCasts.get_children():
 			if raycast.is_colliding():
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -16,4 +16,10 @@ func _onDiceRollFinished(die, number):
 	
 	if (dices.all(func(dice): return dice.is_sleeping())):
 		isRolling = false
-	
+		_moveToStartPosition()
+
+func _moveToStartPosition():
+	for dice in dices:
+			var tween = get_tree().create_tween()
+			tween.tween_property(dice, "rotation", Vector3(dice.rotation.x, 0, dice.rotation.z), 0.3)
+			tween.tween_property(dice, "position", Vector3(dice.StartPosition.x, 1, dice.StartPosition.z), 0.3)
