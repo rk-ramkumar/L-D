0dad0bde82fc948c239478686d87e0ed2b8d9977
ram <rramkumar1306@gmail.com>
2024-05-25 22:05:03 +0530

    Introduced object find helper.

--- Scripts/Helpers.gd
+++ Scripts/Helpers.gd
@@ -8,3 +8,16 @@ func getRandomNumbers(max = 3) -> Dictionary:
 	var second = randi() % max
 	
 	return {first = first, second = second}
+
+func objectFilter(objects, cb: Callable):
+	var res = []
+	for object in objects:
+		if cb.call(object, objects[object]):
+			res.append(objects[object])
+	return res
+
+func objectFind(objects:Dictionary, key, what) -> Dictionary:
+	for object in objects:
+		if objects[object][key] == what:
+			return objects[object]
+	return {}
