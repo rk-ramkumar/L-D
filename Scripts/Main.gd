22e13a8a947fc3676655584757e9d9e05c840728
ram <rramkumar1306@gmail.com>
2024-05-27 21:56:56 +0530

    Refactored.

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -3,9 +3,8 @@ extends Node3D
 @onready var dices = $Dices.get_children()
 @onready var own = $actors/own
 @onready var opponent = $actors/opponent
-@onready var label_3d = $Label3D
 @onready var multiplayer_synchronizer = $MultiplayerSynchronizer
-@onready var roll_button = $CanvasLayer/Control/RollButton
+@onready var roll_button = $UI/Control/RollButton
 @onready var turn_timer = $TurnTimer
 
 signal rollFinsihed
@@ -13,14 +12,12 @@ signal rollFinsihed
 var dieNumbers = {}
 var isRolling: bool = false
 var colors = {"eren": "green", "mikasa": "yellow"}
-var timeForEachTurn = 5
 
 func _ready():
 	own.set_multiplayer_authority(multiplayer.get_unique_id())
 	opponent.set_multiplayer_authority(multiplayer.get_unique_id())
 	GameManager.roundSwitched.connect(_onRoundSwitched)
 	var player = GameManager.Players[multiplayer.get_unique_id()]
-	label_3d.text = player.Name
 	var opponentTeam = "D" if player.team == "L" else "L"
 	var color = colors[GameManager.teamList[player.team].actor]
 	own.get_node("Label3D").text = player.team
@@ -30,15 +27,6 @@ func _ready():
 func startGame():
 	turn_timer.startTimer()
 	_onRoundSwitched(GameManager.currentPlayerTurn)
-
-func _onDiceRollFinished(die, number):
-	dieNumbers.merge({die.name: number}, true)
-	if (dices.all(func(dice): return dice.is_sleeping())):
-		sendDieInfo.rpc(dieNumbers["FirstDie"] + dieNumbers["SecondDie"])
-		rollFinsihed.emit()
-		isRolling = false
-		_moveToStartPosition()
-		upadatePosition.rpc()
 
 @rpc("any_peer", "call_local", "reliable")
 func sendDieInfo(number):
@@ -61,14 +49,24 @@ func _moveToStartPosition():
 			tween.tween_property(dice, "rotation", Vector3(dice.rotation.x, 0, dice.rotation.z), 0.3)
 			tween.tween_property(dice, "position", Vector3(dice.StartPosition.x, 1, dice.StartPosition.z), 0.3)
 
+func _onRoundSwitched(id):
+	if id == GameManager.Players[multiplayer.get_unique_id()].id:
+		roll_button.disabled = false
+	else:
+		roll_button.disabled = true
+
 func _on_roll_button_pressed():
 	if !isRolling:
 		for die in dices:
 			die.roll.rpc()
 		turn_timer.stopTimer.rpc_id(1)
 
-func _onRoundSwitched(id):
-	if id == GameManager.Players[multiplayer.get_unique_id()].id:
-		roll_button.disabled = false
-	else:
-		roll_button.disabled = true
+func _onDiceRollFinished(die, number):
+	dieNumbers.merge({die.name: number}, true)
+	if (dices.all(func(dice): return dice.is_sleeping())):
+		sendDieInfo.rpc(dieNumbers["FirstDie"] + dieNumbers["SecondDie"])
+		rollFinsihed.emit()
+		isRolling = false
+		_moveToStartPosition()
+		upadatePosition.rpc()
+
