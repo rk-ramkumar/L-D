d13c5642a652f2f8a28125e19d4aa94735ec06ff
ram <rramkumar1306@gmail.com>
2024-06-01 02:35:33 +0530

    Refactored.

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -5,7 +5,7 @@ signal roundSwitched(id)
 var Players = {}
 var playerLoaded: int = 0
 var availableId = [1,2, 3, 4]
-var teamList = { "L": {}, "D": {} }
+var teamList = { "L": {"actors": []}, "D": {"actors": []} }
 var tiles = {}
 var gameOver: bool = false
 var currentPlayerTurn: int = 1:
