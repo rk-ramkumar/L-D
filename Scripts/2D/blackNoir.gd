7f484ecc70d313800b3dbeb12cbebcc97062df71
ram <rramkumar1306@gmail.com>
2024-08-25 04:34:44 +0530

    Implemented character touch movement logic.

--- Scripts/2D/blackNoir.gd
+++ Scripts/2D/blackNoir.gd
@@ -15,10 +15,17 @@ func _input(event):
 		var angle = snappedf(mouse_pos.angle(), (PI/4)) / (PI/4)
 		angle = wrapi(int(angle), 0, 8)
 		_update_animation("{direction}_run".format({direction = directions[angle]}))
+		var pos = get_global_mouse_position()
+		_lerp_to_pos(pos)
+		pos = tilemap.local_to_map(pos)
+		tilemap.set_cell(0, pos, 2, Vector2i.ZERO)
+		print(pos)
 
 func _lerp_to_pos(pos):
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", pos, 1)
+	var time_delay = global_position.distance_to(pos) / SPEED
+	print(time_delay)
+	tween.tween_property(self, "position", pos, time_delay)
 
 func cartesian_to_isometric(cartesian_position: Vector2) -> Vector2:
 	var isometric_x = cartesian_position.x - cartesian_position.y
