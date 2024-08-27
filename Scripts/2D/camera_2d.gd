2c8b2227f929eed8f6a257085a0cf1d2e4028ad5
ram <rramkumar1306@gmail.com>
2024-08-27 21:13:09 +0530

    Refactored camera pan logic.

--- Scripts/2D/camera_2d.gd
+++ Scripts/2D/camera_2d.gd
@@ -9,13 +9,18 @@ var min_zoom = 0.5
 var max_zoom = 1.5
 var drag_start = Vector2()
 var dragging = false
+var touch_indexes = {}
+var start_distance = 0
 
 func _ready():
 	set_camera_limit()
 
 func _input(event):
+	if event is InputEventScreenTouch:
+		_handle_touch(event)
 	if event is InputEventScreenDrag:
-		move_camera(event.relative)
+		_handle_drag(event)
+	
 	elif event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			zoom = Vector2(zoom.x * 0.9, zoom.y * 0.9)
@@ -28,17 +33,28 @@ func set_camera_limit():
 	var tile_used_size = tile_map.get_used_rect().size
 	var tile_size = tile_map.tile_set.tile_size / 2
 	var tile_limit = tile_size  * tile_used_size
-	limit_left = -tile_limit.x 
-	limit_right = tile_limit.x - tile_used_size.x
+	limit_left = -tile_limit.x - tile_size.x
+	limit_right = tile_limit.x + tile_size.x
 	limit_top = -tile_limit.y
 	limit_bottom = tile_limit.y
 
-func move_camera(position_offset):
-	var new_pos = position - position_offset
-	# Clamp the camera position to the map boundaries
-	var max_width = map_width - viewport_size.x
-	var max_height = map_height - viewport_size.y
-	new_pos.x = clamp(new_pos.x, -max_width, max_width)
-	new_pos.y = clamp(new_pos.y, 0, max_height)
+func _handle_touch(event: InputEventScreenTouch):
+	if event.pressed:
+		touch_indexes[event.index] = event.position
+	else:
+		touch_indexes.erase(event.index)
 
-	position = new_pos
+	if touch_indexes.size() == 2:
+		var points = touch_indexes.values()
+		start_distance = points[0].distance_to(points[1])
+
+	elif touch_indexes.size() < 2:
+		start_distance = 0
+
+func _handle_drag(event: InputEventScreenDrag):
+	if touch_indexes.size() == 1:
+		position -= event.relative
+	if touch_indexes.size() == 2:
+		touch_indexes[0].distance_to(touch_indexes[1])
+		# Clamp the zoom level
+		zoom = Vector2(clamp(zoom.x, min_zoom, max_zoom), clamp(zoom.y, min_zoom, max_zoom))
