71812b3ea84b66895db1d0c1c15a3c3dc883b19c
ram <rramkumar1306@gmail.com>
2024-09-01 04:15:18 +0530

    Added exit_popup resource to load asynchronously,

--- Scripts/loading.gd
+++ Scripts/loading.gd
@@ -1,34 +1,62 @@
 extends Control
 
-var _progress = [0.0]
+var _progress = []
 var _loading_complete = false
-var scene_path = "res://Scenes/2D/play_ground.tscn"
+var resource_paths = [
+	"res://Scenes/2D/play_ground.tscn",
+	"res://Scenes/exit_popup.tscn"
+]
 @onready var progress_bar = $ProgressBar
 
 func _ready():
-	_load_tilemap_async(scene_path)
+	_load_resources(resource_paths)
 
-func _load_tilemap_async(resource_path: String):
-	var error = ResourceLoader.load_threaded_request(resource_path)
-	if error == OK:
+func _load_resources(resource_paths):
+	var errors = resource_paths.map(func(resource_path):
+			_progress.append(0.0)
+			return ResourceLoader.load_threaded_request(resource_path)
+			)
+
+	if errors.any(func(error): return error == OK):
 		set_process(true)
+
+func _stage_handler(stage, progressCB, loadedCB):
+	match stage:
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			progressCB.call()
+		ResourceLoader.THREAD_LOAD_LOADED:
+			loadedCB.call()
+		ResourceLoader.THREAD_LOAD_FAILED:
+			printerr("Loading resource failed!")
 
 func _process(_delta):
 	if  not _loading_complete:
-		var stage = ResourceLoader.load_threaded_get_status(scene_path, _progress)
-		_progress[0] *= 100
-		match stage:
-			ResourceLoader.THREAD_LOAD_IN_PROGRESS:
-				update_loading_screen(_progress[0])
-			ResourceLoader.THREAD_LOAD_LOADED:
-				_on_loading_complete()
-			ResourceLoader.THREAD_LOAD_FAILED:
-				printerr("Loading resource failed!")
+		for resource_path in resource_paths:
+			var stage = ResourceLoader.load_threaded_get_status(resource_path, _progress)
+			_progress[0] *= 100
+			match stage:
+				ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+					update_loading_screen(_progress[0])
+			
+			match resource_path:
+				"res://Scenes/2D/play_ground.tscn":
+					match stage:
+						ResourceLoader.THREAD_LOAD_LOADED:
+							_on_loading_complete(ResourceLoader.load_threaded_get(resource_path))
+				"res://Scenes/exit_popup.tscn":
+					if stage == ResourceLoader.THREAD_LOAD_LOADED:
+						var exit_popup = ResourceLoader.load_threaded_get(resource_path).instantiate()
+						exit_popup.size = get_viewport_rect().size
+						get_tree().root.add_child(exit_popup)
+				_:
+					if stage == ResourceLoader.THREAD_LOAD_FAILED:
+						printerr("Loading resource failed!")
+
 
 func update_loading_screen(progress: float):
 	progress_bar.value = lerp(progress_bar.value, progress, 0.1)
 
-func _on_loading_complete():
+func _on_loading_complete(resouce):
 	_loading_complete = true
 	set_process(false)
 	var tween = get_tree().create_tween()
@@ -39,9 +67,5 @@ func _on_loading_complete():
 		0.5
 	)
 	tween.tween_callback(
-		get_tree().change_scene_to_packed.bind(
-			ResourceLoader.load_threaded_get(scene_path)
-		)
+		get_tree().change_scene_to_packed.bind(resouce)
 	)
-	tween.tween_callback(tween.kill)
-	_progress[0] = 0
