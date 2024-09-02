9909fa297a02243b828f1bfcdb04f8044cded967
ram <rramkumar1306@gmail.com>
2024-09-02 19:55:37 +0530

    Added otions to custom override for custom buttons.

--- Components/custom_button.gd
+++ Components/custom_button.gd
@@ -3,8 +3,15 @@ extends Button
 signal clicked
 @onready var audio_stream_player = $AudioStreamPlayer
 
+@export_group("custom override")
+@export var to_caps = true
+@export var font_color: Color = Color.WHITE
+
 func _ready():
-	text = text.to_upper()
+	if !has_theme_color_override("font_color"):
+		add_theme_color_override("font_color", font_color)
+	if to_caps:
+		text = text.to_upper()
 	pressed.connect(_on_pressed)
 
 func _on_pressed():
