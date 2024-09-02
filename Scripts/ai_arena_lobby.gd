e50146b074aaf4e6ba5bd0fa147f08355a3c7263
ram <rramkumar1306@gmail.com>
2024-09-02 23:36:35 +0530

    Added mode selection functionality.

--- Scripts/ai_arena_lobby.gd
+++ Scripts/ai_arena_lobby.gd
@@ -1,17 +1,33 @@
 extends Control
 
-@onready var solo_mode_button = $ModeSelectionContainer/SoloModeButton
-@onready var duo_mode_button = $ModeSelectionContainer/DuoModeButton
+@onready var popup = $ModeSelectPopup
+@onready var mode_select_button = $ModeSelectPanel/ModeSelectButton
+@onready var v_box_container = $LeftBoxContainer/VBoxContainer
 
-var current_mode = "solo"
+var current_mode: String: 
+	set(newMode):
+		current_mode = newMode
+		mode_select_button.text = current_mode
+		_update_container()
 
 func _ready():
-	solo_mode_button.disabled = true
+	current_mode = "Solo"
+	popup.size = get_viewport_rect().size
+	get_viewport().size_changed.connect(_on_viewport_changed)
 
 func _on_solo_mode_button_clicked():
-	current_mode = "solo"
-	solo_mode_button.disabled = true
+	current_mode = "Solo"
+	popup.visible = false
 
 func _on_duo_mode_button_clicked():
-	current_mode = "double"
-	duo_mode_button.disabled = true
+	current_mode = "Duo"
+	popup.visible = false
+
+func _on_mode_select_button_clicked():
+	popup.visible = true
+
+func _on_viewport_changed():
+	popup.size = get_viewport_rect().size
+
+func _update_container():
+	pass
