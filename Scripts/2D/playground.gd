fa1df90916769721bcfc526e103731f9bdbb2220
ram <rramkumar1306@gmail.com>
2024-09-03 23:36:12 +0530

    Added logic to get clicked tile data.

--- Scripts/2D/playground.gd
+++ Scripts/2D/playground.gd
@@ -1,6 +1,21 @@
 extends Node2D
 
+signal moveMade
+@onready var turn_timer = %TurnTimer
+@onready var ui = $CanvasLayer/UI
+@onready var tile_map = $TileMap
 
-# Called when the node enters the scene tree for the first time.
+enum tile_layer{
+	FLOOR,
+	BLOCKS,
+	PLANT
+}
+var turnTime = 60
+
 func _ready():
-	pass # Replace with function body.
+	turn_timer.start()
+	
+func get_clicked_tile_data(layer_name, layer_id = tile_layer.FLOOR):
+	var clicked_cell = tile_map.local_to_map(tile_map.get_local_mouse_position())
+	var data = tile_map.get_cell_tile_data(layer_id, clicked_cell)
+	return data.get_custom_data(layer_name) if data else null
