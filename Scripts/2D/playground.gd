dd2c69ac4630be5a6c4833e78cedd1bfe2f0218a
ram <rramkumar1306@gmail.com>
2024-09-04 01:40:27 +0530

    Improved get tile data logic.

--- Scripts/2D/playground.gd
+++ Scripts/2D/playground.gd
@@ -14,8 +14,16 @@ var turnTime = 60
 
 func _ready():
 	turn_timer.start()
-	
+
 func get_clicked_tile_data(layer_name, layer_id = tile_layer.FLOOR):
 	var clicked_cell = tile_map.local_to_map(tile_map.get_local_mouse_position())
-	var data = tile_map.get_cell_tile_data(layer_id, clicked_cell)
-	return data.get_custom_data(layer_name) if data else null
+	var surrounding_cells = tile_map.get_surrounding_cells(clicked_cell)
+	for coords in surrounding_cells:
+		var data = tile_map.get_cell_tile_data(layer_id, coords)
+		if data != null:
+			return data.get_custom_data(layer_name)
+	return null
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		prints(get_clicked_tile_data("can_walk", tile_layer.BLOCKS))
