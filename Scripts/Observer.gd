b4213bad9f4bfe566451867344ba500bfa33120e
ram <rramkumar1306@gmail.com>
2024-09-17 16:28:02 +0530

    Introduced observer script to handle signals.

---
+++ Scripts/Observer.gd
@@ -0,0 +1,13 @@
+extends Node
+
+signal roll_completed
+signal roll_failed
+signal move_completed
+signal move_failed
+signal next_turn
+signal piece_selected
+signal turn_started
+signal game_won
+signal game_over
+signal invalid_move_attempted
+signal piece_captured
