083b386869bced39c30bf9a8a041f27278ae4ce1
ram <rramkumar1306@gmail.com>
2024-09-21 23:24:17 +0530

    Remove unused code.

--- Scripts/players_card.gd
+++ Scripts/players_card.gd
@@ -1,6 +1,5 @@
 extends Panel
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var texture_rect = $TextureRect
 @onready var label = $Label
 var player = {}
@@ -9,12 +8,3 @@ func _ready():
 	label.text = player.get("name", "s")
 	if player.has("icon"):
 		texture_rect.texture = load(player.icon)
-	Observer.turn_started.connect(_on_turn_started)
-
-func _on_turn_started():
-	if GameManager.currentPlayerTurn == player.id:
-		animated_sprite_2d.visible = true
-		animated_sprite_2d.play("default")
-	else:
-		animated_sprite_2d.visible = false
-		animated_sprite_2d.stop()
