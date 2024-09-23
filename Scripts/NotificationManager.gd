1baa9fe38fdfb1835ee582b574d0aa1b67f40c89
ram <rramkumar1306@gmail.com>
2024-09-24 02:26:04 +0530

    Bug fix.

--- Scripts/NotificationManager.gd
+++ Scripts/NotificationManager.gd
@@ -4,4 +4,4 @@ extends Node
 func _notification(what):
 	match what:
 		NOTIFICATION_WM_GO_BACK_REQUEST:
-			get_tree().root.get_node("exit_popup").visible = true
+			get_tree().root.get_node("ExitPopup").visible = true
