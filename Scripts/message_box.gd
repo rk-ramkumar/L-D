fbadb85418f7c888424f8402a206e3fcc5ac26f0
ram <rramkumar1306@gmail.com>
2024-09-23 16:08:16 +0530

    Bug fix.

--- Scripts/message_box.gd
+++ Scripts/message_box.gd
@@ -3,7 +3,7 @@ extends HBoxContainer
 @onready var animation_player = $AnimationPlayer
 
 func _ready():
-	animation_player.play_backwards("open")
+	animation_player.play("RESET")
 
 func _on_button_toggled(button_pressed):
 	if button_pressed:
