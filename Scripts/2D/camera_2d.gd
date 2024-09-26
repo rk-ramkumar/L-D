c75cd9e696735fe519387788efe9e697e99242a7
ram <rramkumar1306@gmail.com>
2024-09-26 23:05:24 +0530

    Bug fix.

--- Scripts/2D/camera_2d.gd
+++ Scripts/2D/camera_2d.gd
@@ -1,9 +1,6 @@
 extends Camera2D
 
-@export var map_width = 44 * 64  # Assuming 64 pixels per tile
-@export var map_height = 44 * 32  # Assuming 32 pixels per tile
 @onready var tile_map = $"../TileMap"
-@onready var viewport_size = get_viewport().size
 
 var min_zoom = 0.5
 var max_zoom = 1.5
@@ -16,19 +13,17 @@ var center_position
 
 func _ready():
 	set_camera_limit()
-	adjust_camera_to_fit_tilemap()
-	get_viewport().size_changed.connect(adjust_camera_to_fit_tilemap)
 
 func adjust_camera_to_fit_tilemap():
 	var viewport_rect = get_viewport_rect().size
 	var screen_ratio = viewport_rect.x / viewport_rect.y
 
 	if screen_ratio > target_ratio:
-		zoom.x = min_zoom * screen_ratio / target_ratio
-		zoom.y = min_zoom
+		zoom.x = 1 * screen_ratio / target_ratio
+		zoom.y = 1
 	else:
-		zoom.y = min_zoom * target_ratio / screen_ratio
-		zoom.x = min_zoom
+		zoom.y = 1 * target_ratio / screen_ratio
+		zoom.x = 1
 
 func _input(event):
 	if event is InputEventScreenTouch:
@@ -39,11 +34,11 @@ func _input(event):
 func set_camera_limit():
 	var tile_used_size = tile_map.get_used_rect().size
 	var tile_size = tile_map.tile_set.tile_size / 2
-	var tile_limit = tile_size  * tile_used_size
+	var tile_limit = tile_size  * tile_used_size / 2
 	limit_left = -tile_limit.x - tile_size.x
 	limit_right = tile_limit.x + tile_size.x
-	limit_top = -tile_limit.y
-	limit_bottom = tile_limit.y	
+	limit_top = -tile_limit.y - tile_size.y
+	limit_bottom = tile_limit.y + tile_size.y
 
 func _handle_touch(event: InputEventScreenTouch):
 	if event.pressed:
@@ -79,8 +74,8 @@ func _handle_drag(event: InputEventScreenDrag):
 			new_zoom.y = clamp(new_zoom.y, min_zoom, max_zoom)
 
 			# Adjust camera position to keep the center point under the pinch gesture
-			center_position = (points[0] + points[1]) / 2
-			var zoom_delta = new_zoom / zoom
-			var camera_offset = center_position - position
-			position += camera_offset * (zoom_delta - Vector2.ONE)
+#			center_position = (points[0] + points[1]) / 2
+#			var zoom_delta = new_zoom / zoom
+#			var camera_offset = center_position - position
+#			position += camera_offset * (zoom_delta - Vector2.ONE)
 			zoom = new_zoom
