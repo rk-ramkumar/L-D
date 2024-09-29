57ad34b607ebd5f839ecffe47ef43d0dc3c9faaa
ram <rramkumar1306@gmail.com>
2024-09-30 00:31:55 +0530

    Add power card to carousel.

--- Scripts/players_card_carousel.gd
+++ Scripts/players_card_carousel.gd
@@ -8,6 +8,8 @@ extends Panel
 var cards = []
 var card_scene = preload("res://Scenes/players_card.tscn")
 var empty_card_texture = preload("res://Assets/empty_card.png")
+var power_card_scene = preload("res://Scenes/power_card.tscn")
+var power_cards = {}
 
 func _ready():
 	Observer.power_purchased.connect(_on_power_purchased)
@@ -33,10 +35,11 @@ func card_selected(selected_card):
 
 func add_empty_card(number):
 	for i in number:
-		cards_container.add_child(create_empty_card())
+		cards_container.add_child(create_empty_card(i))
 
-func create_empty_card():
+func create_empty_card(i):
 	var texture_rect = TextureRect.new()
+	texture_rect.name = "EmptyCard_"+str(i)
 	texture_rect.texture = empty_card_texture
 	texture_rect.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	texture_rect.custom_minimum_size = Vector2(100, 100)
@@ -44,4 +47,19 @@ func create_empty_card():
 	return texture_rect
 
 func _on_power_purchased(power):
-	pass
+	if power_cards.has(power.name):
+		power_cards[power.name].card.update_count(1)
+		return
+	power_cards[power.name] = power
+	power_cards[power.name].merge(_add_power_card(power))
+
+func _add_power_card(power):
+	var power_card = power_card_scene.instantiate()
+	power_card.data = power
+	var empty_card = cards_container.find_child("EmptyCard_*", true, false)
+	var card_index = empty_card.get_index()
+	empty_card.queue_free()
+	cards_container.add_child(power_card)
+	cards_container.move_child(power_card, card_index)
+	
+	return {index = card_index, card = power_card}
--- Scripts/power_card.gd
+++ Scripts/power_card.gd
@@ -2,6 +2,14 @@ extends Panel
 
 @onready var power_texture = $MarginContainer/VBoxContainer/PowerTexture
 @onready var card_count = $MarginContainer/VBoxContainer/Panel/CardCount
+var count = 0
+var data = {}
 
 func _ready():
-	pass 
+	power_texture.texture = load(data.image)
+	update_count(1)
+
+func update_count(amount):
+	count += amount
+	if count > 1:
+		card_count.text = "X"+str(count)
