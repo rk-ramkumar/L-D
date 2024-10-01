72b02284418aac10d83fd6b6b2e400c1c537b69e
ram <rramkumar1306@gmail.com>
2024-10-01 18:23:37 +0530

    Refactore.

--- Scripts/power_card.gd
+++ Scripts/power_card.gd
@@ -8,21 +8,26 @@ var count = 0
 var data = {}
 var picked
 var parent
+var selected = false
 
 func _ready():
 	parent = get_parent().get_owner()
+	add_theme_stylebox_override("panel", get_theme_stylebox("panel").duplicate())
 	power_texture.texture = load(data.image)
 	update_count(1)
 
 func update_count(amount):
 	count += amount
 	if count > 1:
-		card_count.text = "X"+str(count)
+		card_count.text = "X"+str(count) 
 
 func _on_gui_input(event):
 	if event is InputEventScreenTouch:
 		if event.is_pressed():
-			select()
+			if selected:
+				deselect()
+			else:
+				select()
 		if event.is_released():
 			if picked:
 				picked = false
@@ -33,12 +38,22 @@ func _on_gui_input(event):
 		_handle_drag(event)
 
 func select():
+	selected = true
 	animation_player.play("select")
+	deselectOtherCard()
+
+func deselect():
+	selected = false
+	animation_player.play_backwards("select")
+
+func deselectOtherCard():
+	for card in parent.power_cards.values():
+		if card.card != self and card.card.selected:
+			card.card.deselect()
 
 func _handle_drag(event):
 	if !picked:
 		picked = true
 		parent.PowerCardHolder.move_start(data)
-		print(count)
 		update_count(-1)
 	parent.PowerCardHolder.update_position()
